ca52a0a6fd5a43f8227a273280cb3aa1c2919295
Ethan Mosbaugh <ethan@replicated.com>
2026-04-08 05:56:59 -0700

    f

---
+++ playwright.config.ts
@@ -0,0 +1,10 @@
+import { defineConfig, devices } from '@playwright/test'
+
+export default defineConfig({
+  testDir: './e2e',
+  timeout: 30_000,
+  use: {
+    baseURL: 'http://localhost:3000',
+    ...devices['Desktop Chrome'],
+  },
+})
